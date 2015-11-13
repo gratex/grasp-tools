18767672a56ece3c8182d3f00ab85ef3c71f4f85
Michal Beliansky <mbeliansky@gratex.com>
2015-11-13 11:47:49 +0100

    [TOOLS] amd alias usage: added options for pretty print (grasp format)
    Usage: [--help|-h] [--pretty] [--unused|-u] [--names|-n] [<path>]+
    	--help|-h            print this help
    	--unused|-u          print only unused aliases
    	--names|-a           print module name for alias
    	--pretty             print grasp output format
    	where every <path> is either file or directory (for recursive searching)

--- src/amd-aliases-usage.js
+++ src/amd-aliases-usage.js
@@ -12,6 +12,7 @@ var args = process.argv.slice(2);
 // parse args
 var displayUnusedOnly = false;
 var displayMidNames = false;
+var prettyPrint = false;
 var DELIM = "|";
 
 args.forEach(function(a) {
@@ -19,14 +20,13 @@ args.forEach(function(a) {
 		displayUnusedOnly = true;
 	} else if (a == "--names" || a == "-n") {
 		displayMidNames = true;
+	} else if (a == "--pretty") {
+		prettyPrint = true;
+	} else if (a == "--help" || a == "-h") {
+		help();
 	} else if (a[0] == "-") {
 		console.error("Unknow option:", a);
-		console.error("Usage: [--unused|-u] [--names|-n] [<path>]+");
-		console.error("   --unused|-u - print only unused aliases");
-		console.error("   --names|-a - print module name for alias");
-		console.error("   where every <path> is either file or directory (for recursive searching)");
-		process.exit(1);
-		return;
+		help();
 	}
 });
 
@@ -34,7 +34,7 @@ var paths = args.filter(function(a) {
 	return a[0] != "-";
 });
 
-if (paths.length) {
+if (!prettyPrint && paths.length) {
 	if (displayMidNames) {
 		console.log(DELIM, "File", DELIM, "MID", DELIM, "Alias", DELIM, "Used Count", DELIM);
 		console.log(DELIM, "----", DELIM, "---", DELIM, "-----", DELIM, "----------", DELIM);
@@ -44,12 +44,15 @@ if (paths.length) {
 	}
 }
 
-require('./grasp')('Output usage aliases (and their counts) for every dojo module', function(ast) { // process AST of file
+require('./grasp')('Output usage aliases (and their counts) for every dojo module', function(ast, log) { // process AST of file
 	"use strict";
 	// <require|define>(   [<packages>]   ,   function(<aliases>) { ... }  )
 	ast.query('call[callee=(#require, #define)]').forEach(function(rd) {
 		var mids = displayMidNames && rd.query('call.args:matches(arr).elements').map(function(mid) {
-			return mid.value;
+			return {
+				ast : mid,
+				name : mid.value
+			};
 		});
 		var aliasesFn = rd.query('call.args:last:matches(func-exp)'); // aliases, needs body and params
 
@@ -63,16 +66,21 @@ require('./grasp')('Output usage aliases (and their counts) for every dojo modul
 					return;
 				}
 
-				// build output
-				var parts = [
-					ast.file
-				];
-				if (displayMidNames) {
-					parts.push(mids[index]);
-				}
-				parts.push(alias.name, cnt);
+				if (prettyPrint) {
+					displayMidNames ? log(mids[index].ast) : log(alias);
+				} else {
 
-				console.log(DELIM, parts.join(" " + DELIM + " "), DELIM);
+					// build output
+					var parts = [
+						ast.file
+					];
+					if (displayMidNames) {
+						parts.push(mids[index].name);
+					}
+					parts.push(alias.name, cnt);
+
+					console.log(DELIM, parts.join(" " + DELIM + " "), DELIM);
+				}
 			});
 		}
 	});
@@ -88,4 +96,14 @@ function _createAstQueryEngine(astBody) {
 			]
 		}, astBody).length;
 	};
+}
+
+function help() {
+	console.error("Usage: [--help|-h] [--pretty] [--unused|-u] [--names|-n] [<path>]+");
+	console.error("   --help|-h		print this help");
+	console.error("   --unused|-u		print only unused aliases");
+	console.error("   --names|-a		print module name for alias");
+	console.error("   --pretty		print grasp output format");
+	console.error("   where every <path> is either file or directory (for recursive searching)");
+	process.exit(1);
 }
